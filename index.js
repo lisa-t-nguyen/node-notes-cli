006b2d4965f3ab1f3b4ce11e689269a03a9cd74a
Ashelle Cancio <vermillion707@gmail.com>
2022-10-13 23:57:50 +0000

    Explored stuff

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const jsonObj = require('./data.json');
 const fs = require('fs');
+let object = {};
 
 fs.readFile('./data.json', 'utf8', (err, data) => {
-  if (err) throw err;
-  // console.log('data.json:', data);
-  for (var i = 0; i < jsonObj.notes.length; i++) {
-    console.log(jsonObj.notes[i]);
-  }
-})
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  } else {
+    // console.log('data.json:', data);
+    // for (var i = 0; i < jsonObj.notes.length; i++) {
+      //   }
+    let arr = [];
+    for (var key in jsonObj) {
+      arr.push(jsonObj[key]);
+    }
+    console.log(arr);
+    return arr;
+    }
+
+  })
