3d040ee29ddf6cb9d58d2dc7f93598c892651200
sharon-tieu <tieusharon@gmail.com>
2022-10-14 17:13:04 +0000

    Converting number string key to a number type in progress.

--- add.js
+++ add.js
@@ -4,15 +4,18 @@ const object = jsonObj.notes;
 const write = fs.writeFile;
 
 const newNote = process.argv[2];
+const objStringify = JSON.stringify(object);
 
 fs.writeFile('./data.json', newNote, 'utf8', (err, data) => {
   if (err) {
     throw err;
   } else {
-    object.assign(newNote);
+    const addNote = Object.assign(objStringify, newNote);
+    const objParse = JSON.parse(addNote);
   }
   // jsonObj.notes[5] = newNote;
   console.log(data);
 })
+
 
 module.exports = write;
