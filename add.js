5bdf7b0c9ed23617a30b97a575b986a3d74218c9
Ashelle Cancio <vermillion707@gmail.com>
2022-10-14 16:40:25 +0000

    Modified add.js.

--- add.js
+++ add.js
@@ -1,2 +1,18 @@
 const jsonObj = require('./data.json');
 const fs = require('fs');
+const object = jsonObj.notes;
+const write = fs.writeFile;
+
+const newNote = process.argv[2];
+
+fs.writeFile('./data.json', newNote, 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  } else {
+    object.assign(newNote);
+  }
+  // jsonObj.notes[5] = newNote;
+  console.log(data);
+})
+
+module.exports = write;
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 const read = require('./read.js');
+const write = require('./add.js');
