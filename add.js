9227b12178fff2b51c7b500a5642e47a81b0f69a
Lisa Nguyen <lisanguyensc@gmail.com>
2022-10-14 17:33:50 +0000

    Converted jsonObj.notes key to a number

--- add.js
+++ add.js
@@ -6,16 +6,24 @@ const write = fs.writeFile;
 const newNote = process.argv[2];
 const objStringify = JSON.stringify(object);
 
-fs.writeFile('./data.json', newNote, 'utf8', (err, data) => {
-  if (err) {
-    throw err;
-  } else {
-    const addNote = Object.assign(objStringify, newNote);
-    const objParse = JSON.parse(addNote);
-  }
-  // jsonObj.notes[5] = newNote;
-  console.log(data);
-})
+const objKey = Object.keys(object);
+
+const numberKey = () => { for (let i = 0; i < objKey.length; i++) {
+  const key = objKey[i];
+  const keyNumber = Number(key);
+  console.log(keyNumber);
+}}
+
+numberKey();
+// fs.writeFile('./data.json', newNote, 'utf8', (err, data) => {
+//   if (err) {
+//     throw err;
+//   } else {
+//     const addNote = Object.assign(objStringify, newNote);
+//     const objParse = JSON.parse(addNote);
+//   }
+//   console.log(data);
+// })
 
 
 module.exports = write;
