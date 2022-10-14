416fab4e9dc9d18cab05d33be79f1a684ee0b3ff
sharon-tieu <tieusharon@gmail.com>
2022-10-14 18:55:58 +0000

    kind of working.

--- add.js
+++ add.js
@@ -1,3 +1,4 @@
+
 const jsonObj = require('./data.json');
 const fs = require('fs');
 const object = jsonObj.notes;
@@ -8,25 +9,54 @@ const objStringify = JSON.stringify(object);
 
 const objKey = Object.keys(object);
 
-// const numberKey = () => { for (let i = 0; i < objKey.length; i++) {
-//   const key = objKey[i];
-//   const keyNumber = Number(key);
-//   console.log(keyNumber);
-// }}
-const nextId = jsonObj.nextId;
-console.log(nextId);
-
-// numberKey();
-fs.writeFile('./data.json', newNote, 'utf8', (err, data) => {
+// NEW CODE BELOW
+fs.writeFile('./data.json', newNote, 'utf8', err => {
   if (err) {
     throw err;
   } else {
+    const nextId = jsonObj.nextId;
+    const stringyId = nextId.toString();
+    console.log(stringyId)
+
+    object[stringyId] = newNote;
+    console.log('object:', object);
     nextId++;
-    const objParse = JSON.parse(addNote);
-
   }
-  console.log(data);
-})
-
+});
 
 module.exports = write;
+
+// const jsonObj = require('./data.json');
+// const fs = require('fs');
+// const object = jsonObj.notes;
+// const write = fs.writeFile;
+
+// const newNote = process.argv[2];
+// console.log('newNote:', newNote);
+// const objStringify = JSON.stringify(object);
+
+// const objKey = Object.keys(object);
+
+// // converting property to a number
+// // const numberKey = () => { for (let i = 0; i < objKey.length; i++) {
+// //   const key = objKey[i];
+// //   const keyNumber = Number(key);
+// //   console.log(keyNumber);
+// // }}
+// const nextId = jsonObj.nextId;
+// console.log('nextId:', nextId);
+// // numberKey();
+
+// fs.writeFile('./data.json', newNote, 'utf8', err => {
+//   if (err) {
+//     throw err;
+//   } else {
+//     jsonObj.object[nextId] = newNote;
+//     const objParse = JSON.parse(addNote);
+//     console.log(objParse);
+//     nextId++;
+//   }
+//   // console.log(data);
+// })
+
+// module.exports = write;
